fc6de8afb624b544a51fde2254ce69ea1de647e1
장재영 <jangjaeyeong@jangjaeyeong-ui-MacBookPro.local>
2025-07-19 19:02:55 +0900

    initial setting eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+  },
+};
