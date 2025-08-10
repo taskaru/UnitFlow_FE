b01c80c94fe171e3f1216d9cb41190391543938f
PickyPeople <jjyjjh33@gmail.com>
2025-08-10 23:37:34 +0900

    [DEV1001] デザイン待ちのログイン画面完成

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -2,10 +2,10 @@
 div.min-h-screen.flex.items-center.justify-center.bg-gradient-to-br.from-blue-50.to-blue-100.p-4
   .bg-white.shadow-xl.rounded-2xl.p-8.w-full.max-w-md.transition-all.duration-300
     h2.text-3xl.font-bold.text-center.text-gray-800.mb-6 ログイン
-    form.space-y-5(@submit.prevent="handleLogin")
+    form(@submit.prevent="handleLogin")
       div
         label(for="email").block.text-sm.font-medium.text-gray-700 メール
-        input#email.w-full.mt-1.px-4.py-2.rounded-lg.border.border-gray-300(
+        input#email.w-full.mt-1.px-4.py-2.rounded-lg.border.border-gray-300.mb-3(
           v-model="state.email"
           type="email"
           required
@@ -14,18 +14,30 @@ div.min-h-screen.flex.items-center.justify-center.bg-gradient-to-br.from-blue-50
         )
       div
         label(for="password").block.text-sm.font-medium.text-gray-700 パスワード
-        input#password.w-full.mt-1.px-4.py-2.rounded-lg.border.border-gray-300(
+        input#password.w-full.mt-1.px-4.py-2.rounded-lg.border.border-gray-300.mb-7(
           v-model="state.password"
           type="password"
           required
           placeholder="••••••••"
           class="focus:ring-2 focus:ring-blue-500 focus:outline-none"
         )
-      button.w-full.text-white.py-2.px-4.rounded-lg.shadow-md.transition-transform.transform(
+      button.w-full.text-white.py-2.px-4.rounded-lg.shadow-md.transition-transform.transform.mb-5.bg-blue-600(
         type="submit"
-        class="bg-blue-600 hover:bg-blue-700 hover:scale-105"
+        class="hover:bg-blue-700 hover:scale-105"
       )
         | ログイン
+      div.w-full.text-gray-500.py-2.px-4.rounded-lg.shadow-md.transition-transform.transform.mb-5.flex.items-center.justify-center.space-x-2.cursor-pointer(
+        class="hover:scale-105"
+      )
+        img(src="../../assets/i-google.svg" alt="Google" class="w-5 h-5 ")
+        span Googleログイン
+      button.w-full.text-white.py-2.px-4.rounded-lg.shadow-md.transition-transform.transform.mb-5.bg-gray-600(
+        type="button"
+        class="hover:bg-gray-700 hover:scale-105"
+      )
+        | サインアップ
+      div.w-full.test-white.flex.items-center.justify-center.text-gray-400.underline.decoration-gray-400.cursor-pointer
+        | パスワード再設定
 </template>
 
 <script lang="ts">
